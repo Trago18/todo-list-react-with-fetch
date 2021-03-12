49d907022a43385c0651008973a08daef7e151c0
trago18 <franksolorc@hotmail.com>
2021-03-12 01:19:58 -0600

    Add files

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,41 +1,110 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Todos.css'
 
 const Todos = () => {
 
-    const [value, setValue] = useState([]);
+    const [todo, setTodo] = useState([]);
+
+    let url = "https://assets.breatheco.de/apis/fake/todos/user/trago18";
+
+    window.onload = function () {
+        fetch(url)
+            .then(response => { return response })
+            .then(data => {
+                if (data.status === 404) {
+                    let todos = [];
+                    let options = {
+                        method: "POST",
+                        body: JSON.stringify(todos),
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    };
+                    fetch(url, options)
+                        .then(response => { return response.json() })
+                        .then(data => {
+                            console.log('newTodo',data)
+                        })
+                } else {
+                    fetch(url)
+                        .then(response => { return response.json() })
+                        .then(data => {
+                            console.log('loadedTodo',data)
+                            let todos = [];
+                            data.forEach((value) => {
+                                todos.push(value.label)
+                            });
+                            setTodo(todos);
+                        })
+                }
+            });
+    };
+
 
     let addItem = (e) => {
         e.preventDefault();
         if (e.keyCode === 13) {
-            const newValue = e.target.value;
-            const isOnTheList = value.includes(newValue);
+            const newTodo = e.target.value;
+            const isOnTheList = todo.includes(newTodo);
 
             if (isOnTheList) {
                 // message
             } else {
-                newValue !== "" && setValue([...value, newValue]);
+                newTodo !== "" && setTodo([...todo, newTodo]);
             }
             e.target.value = '';
         }
     }
 
+
     let removeItem = (item) => {
-        const newValues = value.filter(value => {
+        const newTodos = todo.filter(value => {
             return value !== item;
         });
-        setValue(newValues);
+        setTodo(newTodos);
     }
 
+    let removeAll = () => {
+        setTodo([]);
+        let options = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch(url, options);
+    }
+
+
+    useEffect(() => {
+        const upTodo = async () => {
+            let todos = [];
+            todo.forEach((value) => {
+                todos.push({ label: value, done: false });
+            })
+            let options = {
+                method: "PUT",
+                body: JSON.stringify(todos),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+            let response = await fetch(url, options).then(res => res.json());
+            console.log(response);
+        }
+        upTodo();
+    }, [todo]);
+
+
     let message;
-    if (value.length === 0) {
+    if (todo.length === 0) {
         message = "No tasks, add a task";
     } else {
         message = "What needs to be done?";
     }
 
 
-    let loop = value.map(item => {
+    let loop = todo.map(item => {
         return (
             <tr key={item} >
                 <td >{item}</td>
@@ -44,8 +113,7 @@ const Todos = () => {
         )
     });
 
-    let items = value.length;
-
+    let items = todo.length;
 
     return (
         <div className="container-fluid">
@@ -55,12 +123,13 @@ const Todos = () => {
                     <tr>
                         <td>
                             <input className="inputClass border-0 bg-light" type="text" placeholder={message} onKeyUp={addItem} />
-                            </td>
+                        </td>
                         <td></td>
                     </tr>
                     {loop}
                     <tr>
                         <td className="text-black-50">{items} item left</td>
+                        <td className='text-end'><button type="button" className='btn btn-secondary btn-sm' onClick={removeAll}>Delete All</button></td>
                     </tr>
                 </tbody>
             </table>
